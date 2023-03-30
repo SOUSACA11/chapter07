01835fdc8673274142f91f56d10ab3c7889b8388
JINNY <sousaca05@gmail.com>
2023-03-30 15:41:37 +0900

    230330_school

---
+++ Overriding/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Overriding
+{
+    class ArmorSuite
+    {
+        public virtual void Initialize()
+        {
+            Console.WriteLine("Armored");
+        }
+    }
+
+    class IronMan : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Repulsor Rays Armed");
+        }
+    }
+
+    class WarMachine : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Double-Barrel Cannons Armed");
+            Console.WriteLine("Micro-Rocket Luncher Armed");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C")
+        }
+    }
+}
